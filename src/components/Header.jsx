3637f54b5020264645f6c41f59588980398a46cd
John Doe <aryanpatel.personal@gmail.com>
2024-09-27 11:15:48 +0530

    dark mode button text update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,19 @@ import React from 'react';
 
 function Header({ handleToggleDarkMode }){
 
+  const handleThemeChange=(e)=>{
+    handleToggleDarkMode((previousDarkMode)=> !previousDarkMode)
+    if(e.target.textContent=="Dark Mode"){
+        e.target.textContent="Light Mode"
+    }
+    else{
+      e.target.textContent="Dark Mode"
+    }
+  }
   return(
     <div className="flex items-center justify-between py-5">
       <h1 className='text-2xl font-bold'>Notes</h1>
-      <button className="border-[1px] border-black bg-[rgb(233,233,233)] hover:bg-gray-400 rounded-xl p-1" onClick={() => handleToggleDarkMode((previousDarkMode)=> !previousDarkMode)}>
+      <button className="border-[1px] border-black bg-[rgb(233,233,233)] hover:bg-gray-400 rounded-xl p-1" onClick={handleThemeChange}>
             Dark Mode
       </button>
       
